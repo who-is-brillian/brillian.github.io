dca5541daf4486c58dfe599cc87d4af8972e83fd
who-is-brillian <brillianmk86@gmail.com>
2024-06-09 13:10:39 +0700

    fungsikan sebuah form melalui spread sheets

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-document
-  .getElementById("contact-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby8QWaxbl1Bwpu9f4oQc8XZDzxNFepjaReRsJvcKkEBXuU6zrRw9tWaedyNBNe1fYIQ/exec";
+const form = document.forms["contact-form-portfolio"];
 
-    const formData = new FormData(event.target);
-    const data = {
-      "entry.2005620554": formData.get("name"),
-      "entry.1045781291": formData.get("email"),
-      "entry.1065046570": formData.get("subject"),
-      "entry.839337160": formData.get("message"),
-    };
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
 
-    fetch(
-      "https://docs.google.com/forms/d/e/1FAIpQLSf00ddyHERDIZAWGWCPpOmLNc6aG0ABhSxPprIy1xfpH4Mlyw/formResponse",
-      {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-        body: new URLSearchParams(data),
-      }
-    )
-      .then(() => {
-        alert("Message sent successfully!");
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-        alert("There was an error sending your message.");
-      });
-  });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // ketika tombol submit di klik
+  // tampilkan tombol loading hilangkan tombol kirim
+  btnLoading.classList.toggle("hidden");
+  btnKirim.classList.toggle("hidden");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      // tampilkan tombol kirim
+      // hilangkan tombol loading
+      btnLoading.classList.toggle("hidden");
+      btnKirim.classList.toggle("hidden");
+      // tampilkan alert
+      myAlert.classList.toggle("hidden");
+      // reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
